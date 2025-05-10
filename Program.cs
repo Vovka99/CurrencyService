696e0828ba997024665cf10db9373c70fc64041a
Volodymyr <mr.vovka2014@gmail.com>
2025-05-10 18:31:22 +0300

    Implement CurrencyRate service
    (cherry picked from commit b0ea69f28268113c13853b2c3582bc358056847e)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using CurrencyService.HttpClients;
+using CurrencyService.Services;
+using CurrencyService.Services.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<ICurrencyRateApiClient, NBUApiClient>();
+builder.Services.AddScoped<ICurrencyRateService, CurrencyRateService>();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
---
+++ Services.Impl/CurrencyRateService.cs
@@ -0,0 +1,53 @@
+using CurrencyService.HttpClients;
+using CurrencyService.Repositories;
+
+namespace CurrencyService.Services.Impl;
+
+public class CurrencyRateService : ICurrencyRateService
+{
+    private const int DATE_SYNC_MONTHS = 3;
+    
+    private readonly ICurrencyRateApiClient _currencyRateApiClient;
+    private readonly ICurrencyRateRepository _currencyRateRepository;
+
+    public CurrencyRateService(ICurrencyRateRepository currencyRateRepository, ICurrencyRateApiClient currencyRateApiClient)
+    {
+        _currencyRateApiClient = currencyRateApiClient;
+        _currencyRateRepository = currencyRateRepository;
+    }
+
+    public async Task<decimal> GetRateAsync(DateTime date, CancellationToken cancellationToken)
+    {
+        var rate = await _currencyRateRepository.GetCurrencyRateAsync(date, cancellationToken);
+        
+        return rate?.Rate ?? 0;
+    }
+
+    public async Task<decimal> GetAverageRateAsync(DateTime start, DateTime end, CancellationToken cancellationToken)
+    {
+        if (end > DateTime.UtcNow.Date)
+        {
+            end = DateTime.UtcNow.Date;
+        }
+
+        return await _currencyRateRepository.GetAverageRateAsync(start, end, cancellationToken);
+    }
+
+    public async Task SyncRatesAsync(CancellationToken cancellationToken)
+    {
+        var latestDate = await _currencyRateRepository.GetLatestDateAsync(cancellationToken);
+        
+        var start = latestDate?.AddDays(1) ?? DateTime.UtcNow.Date.AddMonths(-DATE_SYNC_MONTHS);
+        var end = DateTime.UtcNow.Date;
+
+        if (start <= end)
+        {
+            var rates = await _currencyRateApiClient.GetCurrencyRatesAsync(start, end);
+
+            foreach (var rate in rates)
+            {
+                await _currencyRateRepository.AddCurrencyRateAsync(rate, cancellationToken);
+            }
+        }
+    }
+}
