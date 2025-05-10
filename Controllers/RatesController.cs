4c81805793b83ecb9e467d24f073b5b76806bd9c
Volodymyr <mr.vovka2014@gmail.com>
2025-05-10 18:22:39 +0300

    Endpoint implementation
    (cherry picked from commit 9fc1a6f2581c8487a84d2de1a9a8cf30d9544ae9)

---
+++ Controllers/RatesController.cs
@@ -0,0 +1,47 @@
+using CurrencyService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RatesController : ControllerBase
+{
+    private readonly ICurrencyRateService _currencyRateService;
+
+    public RatesController(ICurrencyRateService currencyRateService)
+    {
+        _currencyRateService = currencyRateService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetCurrent([FromQuery] DateTime? date, CancellationToken cancellationToken)
+    {
+        date ??= DateTime.UtcNow.Date;
+
+        if (date.Value.Date > DateTime.UtcNow.Date)
+        {
+            return BadRequest("Date cannot be greater than current date");
+        }
+        
+        var rate = await _currencyRateService.GetRateAsync(date.Value, cancellationToken);
+        return Ok(rate);
+    }
+
+    [HttpGet("average")]
+    public async Task<IActionResult> GetAverage([FromQuery] DateTime? start, [FromQuery] DateTime? end, CancellationToken cancellationToken)
+    {
+        if (start == null || end == null)
+        {
+            return BadRequest("Start and end dates are required");
+        }
+        
+        if (start.Value > end.Value)
+        {
+            return BadRequest("Start date cannot be greater than the end date");
+        }
+        
+        var averageRate = await _currencyRateService.GetAverageRateAsync(start.Value, end.Value, cancellationToken);
+        return Ok(averageRate);
+    }
+}
