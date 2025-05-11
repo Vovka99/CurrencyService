3d5e387a66533a96c45999dfc552f01e0a2ba045
Volodymyr <mr.vovka2014@gmail.com>
2025-05-11 13:40:22 +0300

    Hangfire shedule instead background service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-using CurrencyService.BackgroundServices;
 using CurrencyService.Database;
 using CurrencyService.Database.Repositories;
 using CurrencyService.HttpClients;
 using CurrencyService.Services;
 using CurrencyService.Services.Impl;
+using Hangfire;
+using Hangfire.PostgreSql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,9 +20,16 @@ builder.Services.AddScoped<IDatabaseInitializer, PostgresDatabaseInitializer>();
 builder.Services.AddSingleton<IDbConnectionFactory, NpgsqlConnectionFactory>();
 builder.Services.AddScoped<ICurrencyRateRepository, CurrencyRateRepository>();
 
-builder.Services.AddHostedService<CurrencyRatesFetcher>();
-
 builder.Services.AddHttpClient();
+
+builder.Services.AddHangfire(configuration =>
+    configuration
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UsePostgreSqlStorage(options =>
+            options.UseNpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
@@ -29,18 +37,24 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "Currency Service");
-    });
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "Currency Service"); });
+    app.UseHangfireDashboard();
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
 
 using var serviceScope = app.Services.CreateScope();
 var dbInitializer = serviceScope.ServiceProvider.GetRequiredService<IDatabaseInitializer>();
 await dbInitializer.InitializeAsync();
+
+BackgroundJob.Enqueue<ICurrencyRateService>(service => service.SyncRatesAsync(CancellationToken.None));
+
+RecurringJob.AddOrUpdate<ICurrencyRateService>(
+    "sync-currency-rates",
+    service => service.SyncRatesAsync(CancellationToken.None),
+    Cron.Daily(15, 31),
+    new RecurringJobOptions { TimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Kyiv") }
+);
 
 app.Run();
